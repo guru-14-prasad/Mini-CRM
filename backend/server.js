6bdc11d5e4a428d8eb3d7882bd365c81bf3c31f5
Guru Prasad S L <guruprasadgp26207@gmail.com>
2026-04-28 07:57:02 +0530

    server.js updated

--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,6 @@ const { Pool } = require("pg");
 const app = express();
 app.use(cors());
 app.use(express.json());
-
-// PostgreSQL connection
 const pool = new Pool({
   user: "postgres",
   host: "localhost",
@@ -14,13 +12,9 @@ const pool = new Pool({
   password: "your_password",
   port: 5432,
 });
-
-// Test route
 app.get("/", (req, res) => {
   res.send("Backend is running 🚀");
 });
-
-// Start server
 app.listen(5000, () => {
   console.log("Server running on port 5000");
 });
